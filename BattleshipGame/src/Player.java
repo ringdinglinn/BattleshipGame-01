cc339697e571bf216c39993b85cf41d72e81abd4
hnord-vdx <henrik.nordgren@vaerdex.ch>
2022-10-31 17:57:15 +0100

    Update Player.java
    Added attributes following conventions and some empty methods

--- BattleshipGame/src/Player.java
+++ BattleshipGame/src/Player.java
@@ -1,7 +1,34 @@
-public class Player {
-    protected Grid grid;
+import java.util.*;
+
+public abstract class Player {
+    protected Grid aGrid;
+    /** Boats that still need to be place */
+    protected List<Boat> aPlaceableBoats;
+    /** Placed boats */
+    protected List<Boat> aBoats;
+    /** Shots that have been fired */
+    protected Shots aShots;
 
     public void printGrid() {
-        grid.printBaseGrid();
+        aGrid.printBaseGrid();
+        // Copy elements from iterable to aPlaceableBoats list
+        new Boats().getBoats().forEach(aPlaceableBoats::add);
+        aBoats = new ArrayList<Boat>(new Boats().getSize());
+        aShots = new Shots();
     }
+
+    public void setBomb() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    /** Returns all shots made by a player */
+    public Iterator<Position> getShots() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    /** Returns all boats of a player */
+    public Iterator<Position> getBoats() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+
 }
