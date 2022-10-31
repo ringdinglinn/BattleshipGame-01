ae405dc437268a6dca5e6fe20b9a5656cba104f7
hnord-vdx <henrik.nordgren@vaerdex.ch>
2022-10-31 17:57:19 +0100

    Update TargetGrid.java

--- BattleshipGame/src/TargetGrid.java
+++ BattleshipGame/src/TargetGrid.java
@@ -1,6 +1,6 @@
 public class TargetGrid extends Grid {
 
-    public char displayPositionState(int x, int y){
-        return ' ';
+    public char displayPositionState(int x, int y) {
+        throw new UnsupportedOperationException("not implemented yet");
     }
 }
