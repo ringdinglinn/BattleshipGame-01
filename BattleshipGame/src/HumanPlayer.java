134f24db2ab4b1e85e5e4fb0c6a3db6f7b220ac1
hnord-vdx <henrik.nordgren@vaerdex.ch>
2022-10-31 17:53:58 +0100

    Update HumanPlayer.java

--- BattleshipGame/src/HumanPlayer.java
+++ BattleshipGame/src/HumanPlayer.java
@@ -1,5 +1,18 @@
 public class HumanPlayer extends Player {
-    public HumanPlayer (){
-        grid = new OceanGrid();
+    public HumanPlayer() {
+        aGrid = new OceanGrid();
+    }
+
+    /**
+     * Computer player doesnt require that method as it sets everything during
+     * initalisation
+     */
+    public void setBoat(Boat pBoat) {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    /** Returns whether all boats have been set */
+    public boolean isAllSet() {
+        throw new UnsupportedOperationException("not implemented yet");
     }
 }
