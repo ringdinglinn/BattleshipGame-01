625bca94b26e74b95ddad43c13810060e8210109
hnord-vdx <henrik.nordgren@vaerdex.ch>
2022-10-31 17:54:39 +0100

    Update Position.java
    We might need to have a method that returns both x and y. Or we define x and y using enums

--- BattleshipGame/src/Position.java
+++ BattleshipGame/src/Position.java
@@ -1,2 +1,22 @@
 public class Position {
+    private final String x;
+    private final String y;
+
+    /**
+     * 
+     * @param x Only valid input [A-J]
+     * @param y Only valid input [0-9]
+     */
+    public Position(String x, String y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public String getY() {
+        return y;
+    }
 }
