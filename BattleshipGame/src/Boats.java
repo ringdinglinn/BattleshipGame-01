bf0ad82a41b33b569f2b045398d236a62c3187ae
hnord-vdx <henrik.nordgren@vaerdex.ch>
2022-10-31 17:55:16 +0100

    Update Boats.java

--- BattleshipGame/src/Boats.java
+++ BattleshipGame/src/Boats.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Boats {
-    private Boat[] boats = {
+    private final Boat[] boats = {
             new Boat(6, 'C'),
             new Boat(4, 'B'),
             new Boat(4, 'B'),
@@ -13,4 +13,20 @@ public class Boats {
             new Boat(2, 'P'),
             new Boat(2, 'P')
     };
+
+    private final int aSize = boats.length;
+
+    /** Returns size of boats array */
+    public int getSize() {
+        return aSize;
+    }
+
+    /** Returns a copy of the boats array as an Iterable<T> */
+    public Iterable<Boat> getBoats() {
+        return new ArrayList<Boat>(Arrays.asList(boats));
+    }
+
+    // ! Im not sure if this class should have the allSunk() method. I think this
+    // ! belongs to player
+
 }
