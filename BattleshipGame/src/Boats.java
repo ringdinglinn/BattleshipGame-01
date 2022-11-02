7d4ba955366303835328c23a421b59a1d71184b7
hnord-vdx <henrik.nordgren@vaerdex.ch>
2022-11-02 16:08:41 +0100

    Migrated to use List<Boat> and not Boat[]

--- BattleshipGame/src/Boats.java
+++ BattleshipGame/src/Boats.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Boats implements Iterable<Boat> {
-    private final Boat[] aBoats = {
+    private final List<Boat> aBoats = Arrays.asList(
             new Boat(6, 'C'),
             new Boat(4, 'B'),
             new Boat(4, 'B'),
@@ -11,23 +11,20 @@ public class Boats implements Iterable<Boat> {
             new Boat(2, 'P'),
             new Boat(2, 'P'),
             new Boat(2, 'P'),
-            new Boat(2, 'P')
-    };
-
-    private final int aSize = aBoats.length;
+            new Boat(2, 'P'));
 
     /** Returns size of boats array */
     public int getSize() {
-        return aSize;
+        return aBoats.size();
     }
 
     /** Returns a copy of the boats array as an Iterable<T> */
     public Iterable<Boat> getBoats() {
-        return new ArrayList<Boat>(Arrays.asList(aBoats));
+        return new ArrayList<Boat>(aBoats);
     }
 
     public Iterator<Boat> iterator() {
-        return Arrays.stream(aBoats).iterator();
+        return aBoats.iterator();
     }
 
 }
