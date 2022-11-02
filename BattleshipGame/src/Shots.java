c51d65a099734fa28abd404fa56f0dd94a53b1f8
hnord-vdx <henrik.nordgren@vaerdex.ch>
2022-11-02 16:05:57 +0100

    Added methods to manipulate and get aShots

--- BattleshipGame/src/Shots.java
+++ BattleshipGame/src/Shots.java
@@ -1,17 +1,35 @@
 import java.util.*;
 
-public class Shots {
-    private Map<String, ShotResult> aShots = Collections.emptyMap();;
+public class Shots implements Iterable<Shot> {
+    private List<Shot> aShots;
 
-    public Map<String, ShotResult> getShots() {
-        return aShots;
+    /**
+     * @pre pPosition != null
+     */
+    public boolean contains(Position pPosition) {
+        for (int i = 0; i < aShots.size(); i++) {
+            if (aShots.get(i).getPosition() == pPosition) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
-     * @param pPosition   accepts only valid positions
-     * @param pShotResult accepts ShotResult enum
+     * @pre pPosition != null && pShotResult != null
+     * @pre aShots.contain(pPosition) != false
      */
-    public void addShot(String pPosition, ShotResult pShotResult) {
-        throw new UnsupportedOperationException("not implemented yet");
+    public void addShot(Position pPosition, ShotResult pShotResult) {
+        aShots.add(new Shot(pPosition, pShotResult));
     }
+
+    /** Returns a copy of the boats array as an Iterable<T> */
+    public Iterable<Shot> getBoats() {
+        return new ArrayList<Shot>(aShots);
+    }
+
+    public Iterator<Shot> iterator() {
+        return aShots.iterator();
+    }
+
 }
